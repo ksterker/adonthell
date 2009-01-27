6064dc277019c5233c415968e2f1371a618a67c1
fr3dc3rv <fr3dc3rv>
2009-01-27 21:24:21 +0000

    Guarantee that s_int32, u_int32 and so on really have the expected MAX and MIN. Uses C99's stdint.h .

--- src/base/types.h
+++ src/base/types.h
@@ -1,5 +1,5 @@
 /*
-   $Id: types.h,v 1.3 2003/12/29 10:01:59 uid66230 Exp $
+   $Id: types.h,v 1.4 2009/01/27 21:24:21 fr3dc3rv Exp $
 
    Copyright (C) 1999/2000/2001/2002/2003 Alexandre Courbot.
    Part of the Adonthell Project http://adonthell.linuxgames.com
@@ -15,41 +15,40 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Adonthell; if not, write to the Free Software 
+   along with Adonthell; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 
-/** 
+/**
  * @file base/types.h
  * @author Alexandre Courbot <alexandrecourbot@linuxgames.com>
  * @brief Declares some basic types.
- */ 
+ */
 
- 
+
 #ifndef BASE_TYPES_H
 #define BASE_TYPES_H
+#include <stdint.h>
 
+// We are assuming CMAKE guarantees the existence of <stdint.h>
 
-// We should put ifdefs here to put the correct int values accordingly to the system type
+/// 32 bits long unsigned
+    typedef uint32_t u_int32;
 
+/// 32 bits long signed
+    typedef int32_t s_int32;
 
-/// 8 bits long unsigned integer
-#define u_int8 unsigned char
+/// 16 bits long unsigned
+    typedef uint16_t u_int16;
 
-/// 16 bits long unsigned integer
-#define u_int16 unsigned short
+/// 16 bits long signed
+    typedef int16_t s_int16;
 
-/// 32 bits long unsigned integer
-#define u_int32 unsigned int
+/// 8 bits long unsigned
+    typedef uint8_t u_int8;
 
-/// 8 bits long signed integer
-#define s_int8 signed char
-
-/// 16 bits long signed integer
-#define s_int16 signed short
-
-/// 32 bits long signed integer
-#define s_int32 signed int
+/// 8 bits long signed
+    typedef int8_t s_int8;
 
 #endif // BASE_TYPES_H
