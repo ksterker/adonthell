ec54c8f5cc48b0498ea5a3c605671c900a93de7b
Kai Sterker <kai.sterker@gmail.com>
2009-02-06 13:12:35 +0000

    UPDATED cmake build to generate and install adonthell.pc file which is required to build tools

---
+++ adonthell.pc.cmake
@@ -0,0 +1,12 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+datadir=${prefix}/share/adonthell
+
+Name: Adonthell
+Description: Role Playing Game Engine
+Version: @VERSION@
+Libs: -L${libdir} -ladonthell_base -ladonthell_python -ladonthell_gfx -ladonthell_input -ladonthell_event -ladonthell_rpg -ladonthell_world
+Cflags: -I${includedir}/adonthell
+
