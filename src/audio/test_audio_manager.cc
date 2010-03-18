5c42f39f0169414d4b109505bfa5a9ba0e7995a0
Josh Glover <jmglov@alhana.(none)>
2010-03-18 11:30:24 +0000

    Added unit tests for all getters and setters to test_audio_manager.cc

--- src/audio/test_audio_manager.cc
+++ src/audio/test_audio_manager.cc
@@ -64,6 +64,60 @@ namespace audio
         EXPECT_EQ(44100, audio_manager::get_audio_rate());
     }
 
+    TEST_F(audio_manager_Test, set_audio_format_Default) {
+        audio_manager::set_audio_format();
+
+        EXPECT_EQ(AUDIO_S16, audio_manager::get_audio_format());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_channels_Default) {
+        audio_manager::set_audio_channels();
+
+        EXPECT_EQ(2, audio_manager::get_audio_channels());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_buffers_Default) {
+        audio_manager::set_audio_buffers();
+
+        EXPECT_EQ(4096, audio_manager::get_audio_buffers());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_mixchannels_Default) {
+        audio_manager::set_audio_mixchannels();
+
+        EXPECT_EQ(16, audio_manager::get_audio_mixchannels());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_rate) {
+        audio_manager::set_audio_rate(27);
+
+        EXPECT_EQ(27, audio_manager::get_audio_rate());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_format) {
+        audio_manager::set_audio_format(27);
+
+        EXPECT_EQ(27, audio_manager::get_audio_format());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_channels) {
+        audio_manager::set_audio_channels(27);
+
+        EXPECT_EQ(27, audio_manager::get_audio_channels());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_buffers) {
+        audio_manager::set_audio_buffers(27);
+
+        EXPECT_EQ(27, audio_manager::get_audio_buffers());
+    }
+
+    TEST_F(audio_manager_Test, set_audio_mixchannels) {
+        audio_manager::set_audio_mixchannels(27);
+
+        EXPECT_EQ(27, audio_manager::get_audio_mixchannels());
+    }
+
 } // namespace{}
 
 
