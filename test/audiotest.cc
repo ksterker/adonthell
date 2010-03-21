c01de48904e12805b62b08dbf5c820bfea598dd6
Josh Glover <jmglov@alhana.(none)>
2010-03-21 08:44:55 +0000

    Initial revision of test/audiotest.cc

---
+++ test/audiotest.cc
@@ -0,0 +1,31 @@
+#include "audio/audio.h"
+#include "audio/audio_manager.h"
+#include "base/configuration.h"
+
+#include <iostream>
+
+using namespace audio;
+
+int main(int argc, char * argv[]) {
+    base::configuration cfg;
+
+    setup(cfg);
+
+    std::cerr << "audio_buffers: "
+              << audio_manager::get_audio_buffers ()
+              << std::endl;
+    std::cerr << "audio_channels: "
+              << audio_manager::get_audio_channels ()
+              << std::endl;
+    std::cerr << "audio_format: "
+              << audio_manager::get_audio_format ()
+              << std::endl;
+    std::cerr << "audio_mixchannels: "
+              << audio_manager::get_audio_mixchannels ()
+              << std::endl;
+    std::cerr << "audio_rate: "
+              << audio_manager::get_audio_rate ()
+              << std::endl;
+
+    return 0;
+}
