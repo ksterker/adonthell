02fa1bb295783485246b8f9a2909b5e1e00e0e24
Kai Sterker <kai.sterker@gmail.com>
2013-03-17 19:42:39 +0100

    ADDED missing file

---
+++ src/gui/gui.cc
@@ -0,0 +1,54 @@
+/*
+   Copyright (C) 2013 Kai Sterker <kai.sterker@gmail.com>
+   Part of the Adonthell Project http://adonthell.linuxgames.com
+
+   Adonthell is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Adonthell is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Adonthell; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/**
+ * @file   gui/gui.cc
+ * @author Kai Sterker <kai.sterker@gmail.com>
+ *
+ * @brief  GUI module init and cleanup
+ */
+
+#include "gui.h"
+#include "fontcache.h"
+
+namespace gui
+{
+    // startup GUI
+    bool init()
+    {
+        FontCache = new font_cache();
+        return FontCache != NULL;
+    }
+
+    // configure GUI
+    void setup(base::configuration & cfg)
+    {
+        // nothing to do for now
+    }
+
+    // shutdown GUI
+    void cleanup()
+    {
+        delete FontCache;
+        FontCache = NULL;
+    }
+
+    /// a single font cache
+    font_cache *FontCache;
+}
